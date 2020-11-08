60de40bec008041b763a64a673cc9541f2d9e7b4
Ian Collington <iancollington@gmail.com>
2020-11-08 09:31:20 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# pre-signup-lambda
---
+++ iac/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+aws cloudformation update-stack --stack-name 'serverlesspizza-pre-signup-lambda-pipeline' \
+	--template-body file://cfn_codepipeline.yml \
+	--region eu-west-1 \
+	--parameters ParameterKey=GitHubToken,ParameterValue=$AWS_GITHUB_TOKEN \
+		ParameterKey=ModuleName,ParameterValue=pre-signup-lambda \
+	--capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+	--profile aws-serverlesspizza-devops
